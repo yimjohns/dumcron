6f37a0120998a0d18d5763d24a8d6e77f1d57635
Yim. Johns <yimjohns2018@gmail.com>
2020-07-26 00:44:21 +0100

    Fixed undefined 'alert' issue

--- task1/question3/coffeecupstatus.js
+++ task1/question3/coffeecupstatus.js
@@ -2,9 +2,9 @@ const coffeeCupSize = 12
 const coffeeTemperature = 65
 
 if(coffeeCupSize > 12 && coffeeTemperature <= 60){
-    alert("The coffecup is larger than 12 fl. oz. and it is piping hot")
+    console.log("The coffecup is larger than 12 fl. oz. and it is piping hot")
 }else if(coffeeCupSize > 12 && coffeeTemperature >= 60){
-    alert("The coffecup is larger than 12 fl. oz. and it is piping hot")
+    console.log("The coffecup is larger than 12 fl. oz. and it is piping hot")
 }else{
-    alert("The coffecup is not larger than 12 fl. oz.")
+    console.log("The coffecup is not larger than 12 fl. oz.")
 }
